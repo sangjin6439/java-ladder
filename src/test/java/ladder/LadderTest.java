431f7e358206420fd6648c1f26a8f67bac49f156
kimihwa <ihwag719@gmail.com>
2024-05-10 16:04:41 +0900

    테스트 코드 작성

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,5 +1,35 @@
 package ladder;
 
-public class LadderTest {
+import model.Ladder;
+import model.Line;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LadderTest {
+    @DisplayName("주어진 높이에 맞게 사다리 생성")
+    @Test
+    void newLadderTest() {
+        int userCount = 5;
+        int height = 6;
+
+        Ladder ladder = new Ladder(userCount, height);
+        List<Line> lines = ladder.getLines();
+        assertEquals(height, lines.size());
+    }
+
+    @DisplayName("사다리의 높이가 2개 미만일 경우 예외 발생")
+    @Test
+    void ValidateLadderHeightTest() {
+        int userCount = 5;
+        int height = 1;
+
+        assertThatThrownBy(() -> new Ladder(userCount, height))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사다리 높이는 최소 2이상이어야 합니다.");
+    }
 }
